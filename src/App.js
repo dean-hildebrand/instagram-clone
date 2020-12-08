653cc2342af15cbc097af6548eb019a22b5aaac3
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-12-08 10:34:13 -0500

    Post component and imported to App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./App.css";
+import Post from './components/Post'
+
 
 function App() {
   return (
     <div className="app">
       <div className="app__header">
-        <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" />
+        <img
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
       </div>
 
-      {/* Header */}
-
-      {/* Posts */}
+     <Post />
       {/* Posts */}
     </div>
   );
---
+++ src/components/Post.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Post() {
+    return (
+        <div className="post">
+            <h3>Username</h3>
+        </div>
+    )
+}
+
+export default Post
