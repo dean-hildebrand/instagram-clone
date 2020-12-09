3ecb378780b15f2e261cc7d1bde8d83f1586956c
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-12-09 08:52:25 -0500

    install material ui and blueprint of Post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import "../css/post.css";
+import Avatar from "@material-ui/core/Avatar";
 
 function Post() {
-    return (
-        <div className="post">
-            <h3>Username</h3>
-        </div>
-    )
+  return (
+    <div className="post">
+      <Avatar
+        className="post__avatar"
+        alt=""
+        // src="/static/images/avatar/1.jpg"
+      />
+      <h3>Username</h3>
+
+      <img
+        className="post__image"
+        src="https://miro.medium.com/max/2400/1*jDIj2SKAE-Bp32owLoHDjw.png"
+        alt=""
+      />
+
+      <h4 className="post__text">
+        <strong>dean_hildebrand</strong> first post on instagram clone
+      </h4>
+    </div>
+  );
 }
 
-export default Post
+export default Post;
