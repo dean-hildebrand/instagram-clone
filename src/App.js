4f6186f978f528c25b199439f1d0ae88d82509fc
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-12-10 13:47:07 -0500

    material ui modal code and button to login open modal

--- src/App.js
+++ src/App.js
@@ -2,9 +2,37 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./components/Post";
 import { db } from "./firebase";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import { Button } from "@material-ui/core";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     db.collection("posts").onSnapshot((snapshot) => {
@@ -19,12 +47,18 @@ function App() {
 
   return (
     <div className="app">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <h2>I am a modal</h2>
+        </div>
+      </Modal>
       <div className="app__header">
         <img
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
           alt=""
         />
       </div>
+      <Button onClick={() => setOpen(true)}>Login</Button>
       {posts.map(({ id, post }) => (
         <Post
           key={id}
